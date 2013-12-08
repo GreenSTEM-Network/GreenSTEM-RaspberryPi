d3102cd82ec2653067817807188bd63b4f1d7d6d
Christopher Nies <nies@localhost.localdomain>
2013-12-08 12:18:29 -0500

    Python script now posting to heroku with live data from sensors

--- serial_test.py
+++ serial_test.py
@@ -4,15 +4,33 @@ import httplib2
 import simplejson as json
 
 h = httplib2.Http()
-linenum = 0
-
-#while 1:
-#    ser = serial.Serial('/dev/ttyUSB0', 57600, timeout=10)
-#    analog = ser.readlines()
-#    print analog
-
 headers = {'Content-Type': 'application/json'}
-data = {'site_id':'1','node_readings':[{'id':'1','timestamp':'2013-12-07 04:05:06','channel':'001','soil1':'1.02','soil2':'1.3','soil3':'1.4','temp':'58','voltage':'1.4'}]}
-body = json.dumps(data)
-uri = "http://solarsunflower.herokuapp.com/dc/"
-resp, content = h.request(uri, "POST", body=body, headers=headers)
+server = "http://solarsunflower.herokuapp.com/dc/"
+
+while 1:
+	now = datetime.datetime.now()
+	yr = now.year
+	mon = now.month
+	dy = now.day
+	hr = now.hour
+	mnt = now.minute
+	sec = now.second
+	if len(str(sec)) == 1:
+		sec = str('0' + str(sec))
+	if len(str(dy)) == 1:
+		dy = str('0' + str(dy))
+	if len(str(mnt)) == 1:
+		mnt = str('0' + str(mnt))
+
+	dte = str(yr) + '-' + str(mon) + '-' + str(dy)
+	tme = str(hr) + ':' + str(mnt) + ':' + str(sec)
+
+	dtme = dte + " " + tme
+
+	ser = serial.Serial('/dev/ttyUSB0', 57600, timeout=9.3)
+	analog = ser.readlines()
+	sensorData = analog[1].rstrip('\r\n')
+	data = {'site_id':'1','node_readings':[{'id':'1','timestamp':str(dtme),'channel':'001','soil1':str(sensorData),'soil2':'1.3','soil3':'1.4','temp':'58','voltage':'1.4'}]}
+	print data
+	body = json.dumps(data)
+	resp, content = h.request(server, "POST", body=body, headers=headers)
