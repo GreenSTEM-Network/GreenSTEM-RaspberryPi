e12dbd13422c2aa3e5fa9dbc2f5734877ed7b41e
Christopher Nies <christophernies@gmail.com>
2014-02-13 19:58:01 -0500

    Modified python code to use comma-separated line, added rainfall data

--- serial_test.py
+++ serial_test.py
@@ -1,14 +1,47 @@
 import serial
 import datetime
 import httplib2
+import urllib
 import simplejson as json
 
+zipCode = '19102'
+API_key = '3545f6916c462e0c8f2e273c87c09fd4'
+h = httplib2.Http()
+headers = {'Content-Type': 'application/json'}
+server = "http://solarsunflower.herokuapp.com/dc/"
+ser = serial.Serial('/dev/ttyUSB0', 57600, timeout=10)
 
 def normalizeDigit(dgt):
     if len(str(dgt)) == 1:
         dgt = str('0' + str(dgt))
     return dgt
 
+def getData(ser):
+    serialData = ser.readline().replace('\x00','').rstrip('\r\n').split(',')
+    return serialData
+
+def getRainfall():
+  http = httplib2.Http()
+  url_base = 'http://api.openweathermap.org/data/2.5/weather'
+  url = '?q=Philadelphia,PA&APPID=3545f6916c462e0c8f2e273c87c09fd4'
+  rainHeaders = {'Content-type': 'application/x-www-form-urlencoded'}
+  response, content = http.request(url_base+url, 'GET', headers=rainHeaders, body='')
+  #print str(response) + '\n'
+  content_dict = json.loads(content)
+  return content_dict['rain']['1h']
+
+def assignData(analog):
+    nodeData = {}
+    nodeData['rainfall'] = getRainfall()
+    try:
+      nodeData['soil1'] = analog[0]
+      nodeData['soil2'] = analog[1]
+      nodeData['soil3'] = analog[2]
+      nodeData['temp'] = analog[3]
+      nodeData['voltage'] = analog[4]
+    except IndexError:
+      nodeData = {}
+    return nodeData
 
 def generateTimestamp():
     now = datetime.datetime.now()
@@ -26,46 +59,30 @@ def generateTimestamp():
 
     return dtme
 
-h = httplib2.Http()
-headers = {'Content-Type': 'application/json'}
-server = "http://solarsunflower.herokuapp.com/dc/"
-ser = serial.Serial('/dev/ttyUSB0', 57600, timeout=10)
-#try to grab and discard the first few data updates - the help text, etc.
-#Still doesn't work. We need to remove all the extraneous Serial.println() statements from the JeeLink code.
-analog = ser.readline()
-analog = ser.readline()
-
-
 while 1:
-  #Set up default variables for each pass
-    #for storing extracted data from JeeLink
-    data = []
-    #constant representing number of lines sent each update
-    NUMBER_OF_LINES = 6
-    #generate timestamp
     dtme = generateTimestamp()
-    #set up a loop for the total number of lines in the update. For each line...
-    for x in range(0,NUMBER_OF_LINES):
-      #read the line
-      analog = ser.readline().rstrip('\r\n')
-      #add it to the list
-      data.append(analog)
-    #assign the data - this is based on the order defined in the JeeLink Receive sketch  
-    soil1 = data[0]
-    soil2 = data[1]
-    soil3 = data[2]
-    temp = data[3]
-    voltage = data[4]
+    analog = getData(ser)
+    data = assignData(analog)
+    #Check nodeData for values
+    if data == {}:
+      continue
     #construct JSON object
-    data = {'site_id': '1',
+    else:
+      packagedData = {'site_id': '1',
             'node_readings': [{'id': '1',
                                'timestamp': str(dtme),
                                'channel': '001',
-                               'soil1': str(soil1),
-                               'soil2': str(soil2),
-                               'soil3': str(soil3),
-                               'temp': str(temp),
-                               'voltage': str(voltage)}]}
-    print data
-#    body = json.dumps(data)
-#    resp, content = h.request(server, "POST", body=body, headers=headers)
+                               'soil1': str(data['soil1']),
+                               'soil2': str(data['soil2']),
+                               'soil3': str(data['soil3']),
+                               'temp': str(data['temp']),
+                               'voltage': str(data['voltage'])}]}#,
+                               #'rainfall':str(data['rainfall'])}]}
+    print packagedData
+    body = json.dumps(data)
+    resp, content = h.request(server, "POST", body=body, headers=headers)
+    print resp
+    print '\n\n\n\n'
+    print '------------'
+    print '\n\n\n\n'
+    print content
